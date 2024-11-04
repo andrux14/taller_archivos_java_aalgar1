ec5ab18e2aa153aade1cfea2f51253a911300c8b
andrux14 <andres.algarra.320@gmail.com>
2024-11-04 16:46:04 -0500

    Subir cambios a git

--- src/ejercicioJavaArchivoPromedio.java
+++ src/ejercicioJavaArchivoPromedio.java
@@ -1,18 +1,23 @@
+// Se importa libreria
 import java.io.*;
 
 public class ejercicioJavaArchivoPromedio {
     public static void main(String[] args) {
 
+        //Se define la ruta del archivo y se crea un objeto File para representar el archivo
         String ruta = "D:/Fundamentos de Programacion/Documentos_Prueba/test2.txt";
         File file = new File(ruta);
 
+        //Se verifica si el archivo exisste
         if (!file.exists()) {
             System.out.println("La ruta del archivo es incorrecta");
             return;
         }
 
+        // Se utiliza dentro de un try un BufferedReader para leer el archivo para que se cierre automaticamente despues de la ejecución
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 
+        // Se inicializan variables de prgrama que va a realizar el calculo del promedio, el numero mayor y el menor
             String linea;
             int suma = 0;
             int i = 0;
@@ -21,9 +26,14 @@ public class ejercicioJavaArchivoPromedio {
 
             while ((linea = br.readLine()) != null) {
 
+        // Se elimian espacios en blanco en los extremos dela linea
                 linea = linea.trim();
                 if (linea.chars().allMatch(Character::isDigit)) {
+
+        // Conviernte a numeros enteros
                     int numero = Integer.parseInt(linea);
+
+        //Calulo de numero mayor, numero menor y promedio
                     suma += numero;
                     i++;
                     if (numero > maximo) {
@@ -46,6 +56,8 @@ public class ejercicioJavaArchivoPromedio {
             } else {
                 System.out.println("El archivo no contiene información valida o está vacío");
             }
+
+        // Excepciones para errores en caso que el archivo no tenga valores validos
         } catch (IOException e) {
 
             System.out.println("Error al leer el archivo: " + e.getMessage());
